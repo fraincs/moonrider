69ef2cfbb32c6635522bd24d2b8d519870e121bf
Francis Thibault <francis.thibault@macbookpro.lan>
2025-01-15 21:29:06 -0500

    updated copy / playlists are now mine

--- src/constants/playlists.js
+++ src/constants/playlists.js
@@ -1,14 +1,14 @@
 module.exports = [
-  {name: 'favorites', author: 'You!', title: 'Your Saved Favorites'},
-  {name: '3966', author: 'BeastSaber', title: 'Songs With Fantastic Flow Vol. 1'},
-  {name: '3967', author: 'BeastSaber', title: 'Songs With Fantastic Flow Vol. 2'},
-  {name: '3968', author: 'BeastSaber', title: 'Songs With Fantastic Flow Vol. 3'},
-  {name: '4134', author: 'BeastSaber', title: 'Songs With Fantastic Flow Vol. 4'},
-  {name: '4135', author: 'BeastSaber', title: 'Songs With Fantastic Flow Vol. 5'},
-  {name: '7483', author: 'BeastSaber', title: 'Maps Of The Weeks'},
-  {name: '4137', author: 'BeastSaber', title: "Curator's Picks of 2020"},
-  {name: '4139', author: 'BeastSaber', title: "Curator's Picks of 2021"},
-  {name: '11584', author: 'BeastSaber', title: "Curator's Picks of 2022"},
-  {name: '4405', author: 'BeastSaber', title: 'Intro to Dance'},
-  {name: '4138', author: 'BeastSaber', title: "Mom's Maps"}
+  {name: 'favorites', author: 'You!', title: 'Favoris'},
+  {name: '729736', author: 'paramour8888', title: 'Disney and Ghibli'},
+  {name: '780009', author: 'murfguy', title: 'Video Games'},
+  {name: '52354', author: 'Whipstickgostop', title: 'Rock'},
+  {name: '193064', author: 'stymiez', title: 'Alternate 90s-00s'},
+  {name: '193713', author: 'SkylerWallace', title: 'Metal/Heavy Rock'},
+  {name: '640969', author: 'effariwhy', title: '80s'},
+  {name: '165769', author: 'VOeVodA', title: "2000 Rock Pack"},
+  {name: '269025', author: 'KingLunalilo', title: "Soundtracks"},
+  {name: '268928', author: 'KingLunalilo', title: "70's-90's"},
+  {name: '785941', author: 'Beatboo', title: 'My favorites 2024'},
+  {name: '451354', author: 'ph1l', title: "Punk Rock"}
 ];
